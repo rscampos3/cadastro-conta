9fb13f8a0aa55f1ef30302f1130efdf1ec26f574
Rafael Campos <rscampos3@gmail.com>
2019-04-25 00:51:52 -0300

    finalizacao do projeto

--- src/main/java/com/example/demo/resource/ContaController.java
+++ src/main/java/com/example/demo/resource/ContaController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.example.demo.model.Conta;
 import com.example.demo.model.Risco;
 import com.example.demo.service.impl.IContaService;
 
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/contas")
 public class ContaController {
